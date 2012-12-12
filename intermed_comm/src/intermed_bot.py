b1ff958bbc29b1ec13bd8c8b0a50a73cb016fbfc
Pedro Castellucci <pedro@pedro-notebook.(none)>
2012-12-11 22:46:52 -0200

    Método 'closestFriendToFlag', que retorna bot mais perto de uma das bandeiras

--- intermed_comm/src/intermed_bot.py
+++ intermed_comm/src/intermed_bot.py
@@ -1,4 +1,4 @@
-# Your AI for CTF must inherit from the base Commander class.  See how this is
+# You AI for CTF must inherit from the base Commander class.  See how this is
 # implemented by looking at the commander.py in the ./api/ folder.
 from api import Commander
 
@@ -12,40 +12,116 @@ from api import Vector2
 
 
 class IntermedCommander(Commander):
-    """
-    Rename and modify this class to create your own commander and add mycmd.Placeholder
-    to the execution command you use to run the competition.
-    """
+	"""
+	Rename and modify this class to create your own commander and add mycmd.Placeholder
+	to the execution command you use to run the competition.
+	"""
 
-    def initialize(self):
-        """Use this function to setup your bot before the game starts."""
-        self.verbose = True    # display the command descriptions next to the bot labels
-        
-    def tick(self):
-        """Override this function for your own bots.  Here you can access all the information in self.game,
-        which includes game information, and self.level which includes information about the level."""
-        
-        # Uncomment to log level information
-        #self.log.debug(LevelHelper(self.level))
-        
-        # Uncomment to log flags and bots information
-        #self.log.debug(GameHelper(self.game))
-        
-        # for all bots which aren't currently doing anything
-        for bot in self.game.bots_available:
-            if bot.flag:
-                # if a bot has the flag run to the scoring location
-                flagScoreLocation = self.game.team.flagScoreLocation
-                self.issue(commands.Charge, bot, flagScoreLocation, description = 'Run to my flag')
-            else:
+	def closestFriendToFlag(self, enemy=True):
+		"""This function returns the closest friend bot to a flag
+		@param enemyFlag - if it is true enemy flag is considered. Friend flag if False
+		@return - The closest bot to the considered flag.		
+		"""
+
+		# Enemy flag position
+		flagPosition = self.game.enemyTeam.flagSpawnLocation
+	
+		# If enemy==True, we must consider our flag
+		if not enemy:
+			flagPosition = self.game.team.flagSpawnLocation			
+
+		# Candidates for bot closest to the considered flag and for minimun squared distance of the flag
+		closestBot = self.game.bots_alive[0]
+		minDist = flagPosition.squaredDistance(closestBot.position)
+
+		# Loop through all the bots that are alive:
+		for bot in self.game.bots_alive:
+
+			candidateDist = bot.position.squaredDistance(flagPosition)
+			# Update to better candidates if needed:
+			if candidateDist < 	minDist:
+				minDist = candidateDist
+				closestBot = bot
+
+		# Return the bot closest to the considered flag
+		return closestBot
+			
+	def defaultStrategy(self):
+
+		# for all bots which aren't currently doing anything
+		for bot in self.game.bots_available:
+		
+			if bot.flag:
+			# if a bot has the flag run to the scoring location
+				flagScoreLocation = self.game.team.flagScoreLocation
+				self.issue(commands.Charge, bot, flagScoreLocation, description = str(self.closestFriendToFlag(False)))
+			else:
                 # otherwise run to where the flag is
-                enemyFlag = self.game.enemyTeam.flag.position
-                self.issue(commands.Charge, bot, enemyFlag, description = 'Run to enemy flag')
+				enemyFlag = self.game.enemyTeam.flag.position
+				self.issue(commands.Charge, bot, enemyFlag, description = str(self.closestFriendToFlag()))
 
-    def shutdown(self):
-        """Use this function to teardown your bot after the game is over, or perform an
-        analysis of the data accumulated during the game."""
-        pass
+		return 
+
+	def testPedro(self):
+
+		# Issueing the bot closest to the flag:
+		#closestBot = self.closestFriendToFlag()[0]
+		#self.issue(commands.Charge, closestBot, self.game.enemyTeam.flag.position, description = 'This is SPARTA!')
+
+		# for all bots which aren't currently doing anything
+		for bot in self.game.bots_available:
+		
+			if bot == self.closestFriendToFlag():
+
+				if bot.flag:
+					# if a bot has the flag run to the scoring location
+					flagScoreLocation = self.game.team.flagScoreLocation
+					self.issue(commands.Charge, bot, flagScoreLocation, description = 'Run Rudolph, Run!')
+				else:
+    	            # otherwise run to where the flag is
+					enemyFlag = self.game.enemyTeam.flag.position
+					self.issue(commands.Charge, bot, enemyFlag, description = 'This is SPARTAA!!')
+
+			else: 
+
+				if bot.position == self.game.team.flag.position:
+					self.issue(commands.Defend, descritption = 'YOU SHALL NOT PASS!!')
+				else: 
+					self.issue(commands.Move, bot, self.game.team.flag.position, description = '\/\/\/\/')
+				
+
+		
+		return 
+
+
+	def initialize(self):
+		"""Use this function to setup your bot before the game starts."""
+		self.verbose = True    # display the command descriptions next to the bot labels
+        
+	def tick(self):
+		"""Override this function for your own bots.  Here you can access all the information in self.game,
+		which includes game information, and self.level which includes information about the level."""
+        
+		# Uncomment to log level information
+		#self.log.debug(LevelHelper(self.level))
+        
+		# Uncomment to log flags and bots information
+		#self.log.debug(GameHelper(self.game))
+
+		# Test strategy here	
+		#self.defaultStrategy()
+		self.testPedro()
+
+		return
+	
+	def shutdown(self):
+		"""Use this function to teardown your bot after the game is over, or perform an
+		analysis of the data accumulated during the game."""
+		pass
+
+	
+	
+
 
 class LevelHelper():
     def __init__(self, level):
